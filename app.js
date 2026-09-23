d0bb3593eb90e7ad595b2693c2a406916f5cdc90
Markysha <12345678+mbrauska-bit@users.noreply.github.com>
2026-09-23 08:56:38 +0200

    Set up Ejs and Express

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-console.console.log(gayporn);
+const express = require("express");
+const app = express();
 
+app.set("view engine","ejs");    
+app.set("views","./views");
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index");;
+})
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.listen(3000,()=>{
+ console.log("Server runnning on http://localhost:3000");
+})
