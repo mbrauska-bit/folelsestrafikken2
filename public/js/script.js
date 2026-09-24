557ac9b8ba072189f0a6699057a30986586f4471
Markysha <12345678+mbrauska-bit@users.noreply.github.com>
2026-09-24 10:07:36 +0200

    Add support choices

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,13 @@ const greenButton = document.querySelector(".green");
 const yellowButton = document.querySelector(".yellow");
 const redButton = document.querySelector(".red");
 
+const miljoarbeiderButton = document.querySelector(".miljoarbeider");
+const radgiverButton = document.querySelector(".radgiver");
+const helsesykepleierButton = document.querySelector(".helsesykepleier");
+const ingenKontaktButton = document.querySelector(".ingen-kontakt");
+
 const trafikk = document.querySelector(".trafikk");
+const stotte = document.querySelector(".stotte");
 const resultat = document.querySelector("#resultat");
 
 
@@ -12,80 +18,105 @@ const sjekkValg = (valg) => {
 
         console.log("Jeg har det greit");
 
+        trafikk.style.display = "none";
+
+        resultat.innerHTML = `
+            <div class="melding">
+                <h2>Godt å høre!</h2>
+                <p>Vi håper du får en fin dag videre 😊</p>
+            </div>
+        `;
+
 
     } else if (valg === "yellow") {
 
         console.log("Jeg har det litt mye akkurat nå");
+
+        trafikk.style.display = "none";
+
+        resultat.innerHTML = `
+            <div class="melding">
+                <h2>Det er helt greit å ha mye å tenke på.</h2>
+                <p>Ta det rolig og prøv å ta én ting om gangen.</p>
+            </div>
+        `;
 
 
     } else if (valg === "red") {
 
         console.log("Jeg trenger støtte");
 
-        // Start animation for the three old buttons
-        trafikk.classList.add("skjul");
-
-
-        // Wait until the animation is finished
-        setTimeout(() => {
-
-            // Remove the old buttons from the page
-            trafikk.style.display = "none";
-
-
-            // Create the new support choices
-            resultat.innerHTML = `
-                <div class="stotte">
-
-                    <h2>Hva ønsker du å gjøre?</h2>
-
-                    <button class="stotte-knapp">
-                        Snakke med en lærer
-                    </button>
-
-                    <button class="stotte-knapp">
-                        Snakke med rådgiver
-                    </button>
-
-                    <button class="stotte-knapp">
-                        Snakke med helsesykepleier
-                    </button>
-
-                    <button class="stotte-knapp">
-                        Jeg vil ikke kontakte noen akkurat nå
-                    </button>
-
-                </div>
-            `;
-
-
-            // Fade in the new choices
-            resultat.classList.add("vis");
-
-        }, 400);
+        trafikk.style.display = "none";
+        stotte.style.display = "flex";
     }
 };
 
 
 greenButton.addEventListener("click", () => {
-
-    console.log("Grønn knappen klikket");
-
     sjekkValg("green");
 });
 
 
 yellowButton.addEventListener("click", () => {
-
-    console.log("Gul knappen klikket");
-
     sjekkValg("yellow");
 });
 
 
 redButton.addEventListener("click", () => {
-
-    console.log("Rød knappen klikket");
-
     sjekkValg("red");
+});
+
+
+miljoarbeiderButton.addEventListener("click", () => {
+
+    stotte.style.display = "none";
+
+    resultat.innerHTML = `
+        <div class="melding">
+            <h2>Miljøarbeider</h2>
+            <p>Du har valgt å snakke med en miljøarbeider.</p>
+            <button>Send forespørsel</button>
+        </div>
+    `;
+});
+
+
+radgiverButton.addEventListener("click", () => {
+
+    stotte.style.display = "none";
+
+    resultat.innerHTML = `
+        <div class="melding">
+            <h2>Rådgiver</h2>
+            <p>Du har valgt å snakke med en rådgiver.</p>
+            <button>Send forespørsel</button>
+        </div>
+    `;
+});
+
+
+helsesykepleierButton.addEventListener("click", () => {
+
+    stotte.style.display = "none";
+
+    resultat.innerHTML = `
+        <div class="melding">
+            <h2>Helsesykepleier</h2>
+            <p>Du har valgt å snakke med en helsesykepleier.</p>
+            <button>Send forespørsel</button>
+        </div>
+    `;
+});
+
+
+ingenKontaktButton.addEventListener("click", () => {
+
+    stotte.style.display = "none";
+
+    resultat.innerHTML = `
+        <div class="melding">
+            <h2>Valget ditt er registrert</h2>
+            <p>Du har valgt å ikke kontakte noen akkurat nå.</p>
+        </div>
+    `;
 });
